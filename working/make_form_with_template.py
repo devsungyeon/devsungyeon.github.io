80262f9eba7ede5cc15a075226d1f6168f95ded5
devsungyeon <sungyeonlee1103@gmail.com>
2021-01-21 21:07:33 +0900

    ing

--- working/make_form_with_template.py
+++ working/make_form_with_template.py
@@ -36,8 +36,14 @@ subjectsL7 = [
     'softwareeng'
 ]
 
-linksubjectsL9 = "../assets/images/civil_service_examinatio/Level9_civil_servant/Level9_"
-linksubjectsL7 = "../assets/images/civil_service_examinatio/Level7_civil_servant/Level7_"
+#linksubjectsL9start = "../assets/images/civil_service_examinatio/Level9_civil_servant/Level9_"
+linksubjectsL9start = "C:/GitHub/devsungyeon.github.io/assets/images/civil_service_examinatio/Level9_civil_servant/Level9_"
+linksubjectsL9tail = "_9L/"
+#linksubjectsL7start = "../assets/images/civil_service_examinatio/Level7_civil_servant/Level7_"
+linksubjectsL7start = "C:/GitHub/devsungyeon.github.io/assets/images/civil_service_examinatio/Level7_civil_servant/Level7_"
+linksubjectsL7tail = "_7L/"
+
+
 
 print("Select the Level you want (7, 9) : ", end="")
 level_input = int(input())
@@ -45,19 +51,42 @@ if level_input not in Level:
     sys.exit(0)
 print("/n==== Subject List ====")
 i = 1
-for name in subjectsL9 if Level[0] == level_input else subjectsL7:
+for name in subjectsL7 if Level[0] == level_input else subjectsL9:
     print(f"{i} : {name}")
     i += 1
 print("Select the subject number you want : ", end="")
 subject_number = int(input())
+subject_name = str(subjectsL9[subject_number-1] if level_input == 9 else subjectsL7[subject_number-1])
 
 print("Select the year you want : ", end="")
 year_input = int(input())
 
-test = "../assets/images/civil_service_examinatio/Level"+level_input+"_civil_servant/Level"+level_input+"_database/2020_7L/2020_7L_1.jpg"
-#open(test)
+link = linksubjectsL9start if level_input == 9 else linksubjectsL7start
+link += subject_name + "/"
+link += str(year_input) + "_" + str(level_input) + "L/"
+link += str(year_input) + "_" + str(level_input) + "L_"
+print(link)
 
+file_name = "yyyy-mm-dd-Level" + str(level_input) + "-" + subject_name + "_y" + str(year_input) + ".md"
+print(file_name)
 
+# Load the file into file_content
+file_content = [ line for line in open('data-d.md') ]
 
+# Overwrite it
+writer = open('data.md','w')
 
+i = 1
+#![2016_9L_1](C:\GitHub\devsungyeon.github.io\assets\images\civil_service_examinatio\Level9_civil_servant\Level9_computerbasic\2016_9L\2016_9L_1.jpg)
+problems_year_format = "#![" + str(year_input) + "_" + str(level_input) + "L_"
+for line in file_content:
+    # Re-write the file at 
+    # each iteration
+    writer.write(line)
 
+    # We search for the correct section
+    if line.startswith(str(i) + "."):
+        section = problems_year_format + str(i) + "](" + link + str(i) + ".jpg)"
+        print(section)
+        writer.write(section)
+writer.close()
---
+++ working/test.py
@@ -0,0 +1,5 @@
+str1 = 'yyyy-mm-dd-Level7-database_y2019.md'
+str2 = 'yyyy-mm-dd-Level7-database_y2019.md'
+
+if str1 == str2 :
+    print("correct")
